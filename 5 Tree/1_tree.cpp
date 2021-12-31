a566cbdb89985afa7186f97e7b7c0a9a58bf98ee
Piyush Agrawal <piyusha1210@gmail.com>
2021-12-31 11:57:23 +0530

    Searching

---
+++ 5 Tree/1_tree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inorder(node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    node *root = new node(4);
+    root->left = new node(2);
+    root->right = new node(6);
+    root->left->left = new node(1);
+    root->left->right = new node(3);
+    root->right->left = new node(5);
+    root->right->right = new node(7);
+    inorder(root); // 1 2 3 4 5 6 7
+    cout << endl;
+    preorder(root); // 4 2 1 3 6 5 7
+    cout << endl;
+    postorder(root); // 1 3 2 5 7 6 4
+    cout << endl;
+
+    return 0;
+}
