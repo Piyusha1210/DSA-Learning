7bf579af31557e3191f9706e85ddf4398d740934
Piyush Agrawal <piyusha1210@gmail.com>
2022-01-28 12:14:18 +0530

    Sorting Modified

--- 1 Searching Sorting/2_sorting.cpp
+++ 1 Searching Sorting/2_sorting.cpp
@@ -15,12 +15,43 @@ void selectionSort(int a[], int n)
     }
 }
 
-void insertionSort(int a[], int n)
-{
-}
-
 void bubbleSort(int a[], int n)
 {
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            if (a[j - 1] > a[j])
+            {
+                swap(a[j - 1], a[j]);
+            }
+        }
+    }
+}
+
+void insertionSort(int a[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int j = i - 1;
+        int t = a[i];
+        while (t < a[j] && j >= 0)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        j++;
+        a[j] = t;
+    }
+}
+
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
@@ -32,11 +63,17 @@ int main()
     {
         cin >> a[i];
     }
-    cout << "Selection Sort " << endl;
-    selectionSort(a, n);
-    cout << "Insertion Sort " << endl;
-    insertionSort(a, n);
-    cout << "Bubble Sort " << endl;
-    bubbleSort(a, n);
+    // cout << "Selection Sort " << endl;
+    // selectionSort(a, n);
+    // printArray(a,n);
+
+    // cout << "Bubble Sort " << endl;
+    // bubbleSort(a, n);
+    // printArray(a, n);
+
+    // cout << "Insertion Sort " << endl;
+    // insertionSort(a, n);
+    // printArray(a, n);
+
     return 0;
 }
---
+++ 1 Searching Sorting/4_quicksort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void quickSort(int a[], int n)
+{
+    
+}
+
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    // cout << "Quick Sort " << endl;
+    // quickSort(a, n);
+    // printArray(a,n);
+
+    return 0;
+}
