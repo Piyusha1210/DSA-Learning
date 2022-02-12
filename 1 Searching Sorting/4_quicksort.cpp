b496048fdcb5adf8396afffbccc7cdbd0eb993f7
Piyush Agrawal <piyusha1210@gmail.com>
2022-02-12 20:36:53 +0530

    Quick Sort Added

--- 1 Searching Sorting/4_quicksort.cpp
+++ 1 Searching Sorting/4_quicksort.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void quickSort(int a[], int n)
+int partition(int a[], int l, int h)
 {
-    
+    int pivot = a[h];
+    int i = l - 1;
+    for (int j = l; j < h; j++)
+    {
+        if (a[j] <= pivot)
+        {
+            i++;
+            swap(a[i],a[j]);
+        }
+    }
+    swap(a[i+1],a[h]);
+    return i+1;
+}
+void quickSort(int a[], int l, int h)
+{
+    if (l < h)
+    {
+        int pos = partition(a, l, h);
+        quickSort(a, l, pos - 1);
+        quickSort(a, pos + 1, h);
+    }
+    return;
 }
 
 void printArray(int a[], int n)
@@ -24,9 +45,9 @@ int main()
     {
         cin >> a[i];
     }
-    // cout << "Quick Sort " << endl;
-    // quickSort(a, n);
-    // printArray(a,n);
+    cout << "Quick Sort " << endl;
+    quickSort(a, 0, n - 1);
+    printArray(a, n);
 
     return 0;
 }
