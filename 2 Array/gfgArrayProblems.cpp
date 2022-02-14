694de7fd1987152c81fed44fe93b925f3bb7c3eb
Piyush Agrawal <piyusha1210@gmail.com>
2022-02-14 21:10:46 +0530

    GFG Array Que Added

--- 2 Array/gfgArrayProblems.cpp
+++ 2 Array/gfgArrayProblems.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define queSolved 3
+
+#define queSolved 5
 
 /*
-1 
+1
 class Solution
 {
 public:
@@ -85,6 +86,59 @@ class Solution
             }
         }
         return miss;
+    }
+};
+*/
+
+/*
+4 Mean of range in array
+https://practice.geeksforgeeks.org/problems/mean-of-range-in-array2123/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=0&page=2&query=category[]ArraysproblemStatusunsolveddifficulty[]0page2category[]Arrays
+
+class Solution{
+    public:
+    vector<int> findMean(int arr[],int queries[],int n,int q)
+    {
+        int sum[n+1];
+        sum[0]=0;
+        for(int i=0; i<n; i++)
+        {
+            sum[i+1] = sum[i]+arr[i];
+        }
+        
+        vector<int> v;
+        for(int i=0; i<q; i+=2)
+        {
+            int p = sum[queries[i+1]+1] - sum[queries[i]];
+            int q = (queries[i+1] - queries[i] + 1);
+            v.push_back(p/q);
+        }
+        return v;
+    }
+
+};
+*/
+
+/*
+5 First Repeating Element 
+https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1/?category[]=Arrays&category[]=Arrays&problemStatus=unsolved&difficulty[]=0&page=1&query=category[]ArraysproblemStatusunsolveddifficulty[]0page1category[]Arrays
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        unordered_map<int, int> m;
+        
+        for(int i=0; i<n; i++)
+        {
+            m[arr[i]]++;
+        }
+        for(int i=0; i<n; i++)
+        {
+            auto it = m.find(arr[i]);
+            if((*it).second > 1)
+                return i+1;
+        }
+        return -1;
     }
 };
 */
