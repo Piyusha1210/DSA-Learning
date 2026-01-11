6312efed94faf56b527ebf2434c5771bcfa9cea3
Piyush Agrawal <piyusha1210@gmail.com>
2026-01-12 00:01:07 +0530

    Fix BiPartite Graph root color bug

--- 13 Graph/1_BFS_DFS/8_bipartiteGraph.cpp
+++ 13 Graph/1_BFS_DFS/8_bipartiteGraph.cpp
@@ -3,45 +3,49 @@ using namespace std;
 
 // https://leetcode.com/problems/is-graph-bipartite/submissions/1321931496/
 
-class Solution {
+class Solution
+{
 public:
     bool bfs(int root, vector<bool> &visited, vector<int> &color, vector<vector<int>> &graph)
     {
         queue<int> qu;
         qu.push(root);
-        while(!qu.empty())
+        color[root] = 0;
+        visited[root] = true;
+        while (!qu.empty())
         {
             int t = qu.front();
             int c = color[t];
             qu.pop();
-            for(auto it: graph[t])
+            for (auto it : graph[t])
             {
-                if(visited[it])
+                if (visited[it])
                 {
-                    if(color[it] == c)
+                    if (color[it] == c)
                         return true;
                 }
                 else
                 {
                     qu.push(it);
                     visited[it] = true;
-                    color[it] = (c+1) % 2;
+                    color[it] = (c + 1) % 2;
                 }
             }
         }
         return false;
     }
 
-    bool isBipartite(vector<vector<int>>& graph) {
+    bool isBipartite(vector<vector<int>> &graph)
+    {
         int n = graph.size();
         vector<int> color(n, -1);
         vector<bool> visited(n, false);
 
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if(!visited[i])
+            if (!visited[i])
             {
-                if(bfs(i, visited, color, graph))
+                if (bfs(i, visited, color, graph))
                     return false;
             }
         }
